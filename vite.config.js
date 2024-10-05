9c78197733c9ffa1af27c59e0402e6a22eeb312b
Anika Islam <anikaislamporoma17@gmail.com>
2024-10-05 19:27:23 +0600

    #

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // Base URL for GitHub Pages (use your repository name)
+  build: {
+    outDir: 'dist', // Default output directory for Vite is 'dist', let's stick with that for now
+  },
+  server: {
+    open: true,  // Automatically opens the browser
+    port: 3000,  // Dev server port
+  }
 })
